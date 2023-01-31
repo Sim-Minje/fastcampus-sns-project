1bd6accf1147dc135d099a2675868643d1c80090
Sim-Minje <smj7918@gmail.com>
2023-01-31 22:57:29 +0900

    ch2- 06. 회원가입 & 로그인 구현

--- src/main/java/com/fastcampus/snsproject/exception/SnsApplicationException.java
+++ src/main/java/com/fastcampus/snsproject/exception/SnsApplicationException.java
@@ -3,16 +3,22 @@ package com.fastcampus.snsproject.exception;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
+
 @Getter
+@AllArgsConstructor
 public class SnsApplicationException extends RuntimeException {
 
     private ErrorCode errorCode;
     private String message;
 
+    public SnsApplicationException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+        this.message = null;
+    }
+
     @Override
     public String getMessage() {
-        if(message == null)
+        if (message == null)
             return errorCode.getMessage();
 
         return String.format("%s. %s", errorCode.getMessage(), message);
